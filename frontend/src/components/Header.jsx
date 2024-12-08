edb96bfabd0944cfa129ed9913eb7c6c3952ffef
abde chkir <abdechkir@example.com>
2024-12-08 22:01:06 +0100

    updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,13 +5,26 @@ import {AcmeLogo} from "../app/fonts/AcmeLogo";
 import { SwipeableDrawer } from '@mui/material';
 import { FaSearch, FaTimes } from 'react-icons/fa';
 import { X } from 'lucide-react';
-
+import { GoSearch } from "react-icons/go";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerBody,
+  DrawerFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isSearchVisible, setIsSearchVisible] = useState(false);
   const [drawerOpen, setDrawerOpen] = useState(false); // State to manage drawer
+  const [drawerSearchOpen, setDrawerSearchOpen] = useState(false); // State to manage drawer
   const searchRef = useRef(null);
+
+
+  const toggleSearchDrawer = (open) => () => {
+    setDrawerSearchOpen(open);
+  };
 
 
   useEffect(() => {
@@ -27,13 +40,26 @@ export default function Header() {
     };
   }, []);
 
-  const toggleSearch = () => {
-    setIsSearchVisible(prevState => !prevState);
-  };
-
   const toggleDrawer = (open) => () => {
     setDrawerOpen(open);
   };
+
+  const searchBar = () => (
+    <div className="w-full flex justify-center items-center bg-white h-28">
+  <div className="w-[50%] flex items-center border rounded-none relative">
+    <input
+      type="text"
+      placeholder="Search"
+      className="w-full pl-8 pr-8 py-2 outline-none"
+    />
+      <GoSearch className="h-5 w-5 absolute left-2 text-gray-500" />
+    <button className="absolute right-2 text-gray-500 hover:text-black">
+      <X onClick={() => setDrawerSearchOpen(false)} strokeWidth={1.25} />
+    </button>
+  </div>
+</div>
+
+  )
 
   const list = () => (
     <div
@@ -72,29 +98,8 @@ export default function Header() {
   const anchors = ['left', 'right', 'top', 'bottom'];
 
   return (
-    <Navbar className="bg-black text-white py-4 font-semibold" onMenuOpenChange={setIsMenuOpen}>
-
-      <NavbarContent className="hidden sm:flex gap-4 -ml-36" justify="center">
-        <div className="relative">
-          {/* Search Icon */}
-          <button onClick={toggleSearch} className="text-white">
-            {isSearchVisible? <FaTimes />: <FaSearch onClick={() => setIsSearchVisible(false)} />}
-          </button>
-
-          {/* Search Input */}
-          <div
-            ref={searchRef}
-            className={`absolute top-[19px] left-0 transition-all duration-500 ease-in-out ${
-              isSearchVisible ? 'w-48 opacity-100' : 'w-0 opacity-0'
-            } overflow-hidden`}
-          >
-            <input
-              type="text"
-              placeholder="Search..."
-              className="p-2 text-gray-800 text-sm focus:outline-none"
-            />
-          </div>
-        </div>
+    <Navbar className="bg-white flex justify-between text-black py-4 font-semibold" onMenuOpenChange={setIsMenuOpen}>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem isActive>
           <Link href="#" aria-current="page">
           CONCEPT
@@ -117,7 +122,7 @@ export default function Header() {
         </NavbarItem>
       </NavbarContent>
       
-      <NavbarContent className="ml-28 mr-96">
+      <NavbarContent className="">
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
@@ -128,22 +133,40 @@ export default function Header() {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent justify="end">
-        <NavbarItem className="flex">
-          <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-4 cursor-pointer mx-2 h-auto">
-            <path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"></path>
-          </svg>
 
-          <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-4 cursor-pointer mx-2 h-auto">
-            <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
-          </svg>
+      <NavbarContent>
+        <NavbarItem className="flex cursor-pointer mr-2">
+          <div onClick={toggleSearchDrawer(true)}>
+            <GoSearch size={22} className="text-gray-700" />
+          </div>
+        </NavbarItem>
+
+        {/* Swipeable Drawer */}
+        <SwipeableDrawer
+          anchor="top"
+          open={drawerSearchOpen}
+          onClose={toggleSearchDrawer(false)}
+          onOpen={toggleSearchDrawer(true)}
+        >
+          {searchBar()}
+        </SwipeableDrawer>
+      </NavbarContent>
+
+      <NavbarContent>
+        <NavbarItem className="flex cursor-pointer">
+            <svg width="20px" height="20px" fill="#000000" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false" className="icon icon-account tw-w-6 tw-h-6" viewBox="0 0 18 19">
+              <path fillRule="evenodd" clipRule="evenodd" d="M6 4.5a3 3 0 116 0 3 3 0 01-6 0zm3-4a4 4 0 100 8 4 4 0 000-8zm5.58 12.15c1.12.82 1.83 2.24 1.91 4.85H1.51c.08-2.6.79-4.03 1.9-4.85C4.66 11.75 6.5 11.5 9 11.5s4.35.26 5.58 1.15zM9 10.5c-2.5 0-4.65.24-6.17 1.35C1.27 12.98.5 14.93.5 18v.5h17V18c0-3.07-.77-5.02-2.33-6.15-1.52-1.1-3.67-1.35-6.17-1.35z" fill="currentColor">
+              </path>
+            </svg>
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent justify="end" className=" -mr-6">
+      <NavbarContent className="">
         <NavbarItem onClick={toggleDrawer(true)} className="-mr-14 flex cursor-pointer relative">
-          <span className="absolute inline-flex items-center px-1 -top-2 -right-3 rounded-full text-xs bg-white text-black">0</span>
-          <svg fill="#fff" xmlns="http://www.w3.org/2000/svg" id="ast-bag-icon-svg" x="0px" y="0px" width="20" height="20" viewBox="826 826 140 140" enableBackground="new 826 826 140 140" style={{ display: 'inline-block' }}><path d="M960.758,934.509l2.632,23.541c0.15,1.403-0.25,2.657-1.203,3.761c-0.953,1.053-2.156,1.579-3.61,1.579H833.424c-1.454,0-2.657-0.526-3.61-1.579c-0.952-1.104-1.354-2.357-1.203-3.761l2.632-23.541H960.758z M953.763,871.405l6.468,58.29H831.77l6.468-58.29c0.15-1.203,0.677-2.218,1.58-3.045c0.903-0.827,1.981-1.241,3.234-1.241h19.254v9.627c0,2.658,0.94,4.927,2.82,6.807s4.149,2.82,6.807,2.82c2.658,0,4.926-0.94,6.807-2.82s2.821-4.149,2.821-6.807v-9.627h28.882v9.627c0,2.658,0.939,4.927,2.819,6.807c1.881,1.88,4.149,2.82,6.807,2.82s4.927-0.94,6.808-2.82c1.879-1.88,2.82-4.149,2.82-6.807v-9.627h19.253c1.255,0,2.332,0.414,3.235,1.241C953.086,869.187,953.612,870.202,953.763,871.405z M924.881,857.492v19.254c0,1.304-0.476,2.432-1.429,3.385s-2.08,1.429-3.385,1.429c-1.303,0-2.432-0.477-3.384-1.429c-0.953-0.953-1.43-2.081-1.43-3.385v-19.254c0-5.315-1.881-9.853-5.641-13.613c-3.76-3.761-8.298-5.641-13.613-5.641s-9.853,1.88-13.613,5.641c-3.761,3.76-5.641,8.298-5.641,13.613v19.254c0,1.304-0.476,2.432-1.429,3.385c-0.953,0.953-2.081,1.429-3.385,1.429c-1.303,0-2.432-0.477-3.384-1.429c-0.953-0.953-1.429-2.081-1.429-3.385v-19.254c0-7.973,2.821-14.779,8.461-20.42c5.641-5.641,12.448-8.461,20.42-8.461c7.973,0,14.779,2.82,20.42,8.461C922.062,842.712,924.881,849.519,924.881,857.492z"></path></svg>
+          <span className="absolute inline-flex items-center px-1 right-0 rounded-full text-xs bg-black text-white">0</span>
+            <svg width="40px" height="40px" fill="#000000" className="icon icon-cart-empty tw-w-14 tw-h-14 tw-text-white" aria-hidden="true" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40">
+            <path d="m15.75 11.8h-3.16l-.77 11.6a5 5 0 0 0 4.99 5.34h7.38a5 5 0 0 0 4.99-5.33l-.78-11.61zm0 1h-2.22l-.71 10.67a4 4 0 0 0 3.99 4.27h7.38a4 4 0 0 0 4-4.27l-.72-10.67h-2.22v.63a4.75 4.75 0 1 1 -9.5 0zm8.5 0h-7.5v.63a3.75 3.75 0 1 0 7.5 0z" fill="currentColor" fillRule="evenodd"></path>
+          </svg>        
         </NavbarItem>
 
         {/* Swipeable Drawer */}
@@ -156,7 +179,6 @@ export default function Header() {
           {list()}
         </SwipeableDrawer>
       </NavbarContent>
-
 
       
       <NavbarMenu>
