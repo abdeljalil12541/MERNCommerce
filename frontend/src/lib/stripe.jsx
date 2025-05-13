14d21e222e181e3e12671b7459c829c6cbdec882
abde chkir <abdechkir@example.com>
2025-05-13 15:11:13 +0100

    adding stripe payment to the checkout page

--- frontend/src/lib/stripe.jsx
+++
@@ -1,5 +0,0 @@
-import 'server-only';
-
-import Stripe from 'stripe';
-
-export const stripe = new Stripe('sk_test_51OpZiJFHs1Gmgtcb4tfLJoVlcY6c15ZasfouYa4U0Pp4688mU7ZpwHrq1NcyjP9jnWnshfsPTFQ41kuZNxDTctLI00WW1PCIuT');
